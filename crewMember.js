e22ea779b60e1ba775233254cdd477ede3704e96
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 22:19:32 +0000

    automatically backed up by learn

--- crewMember.js
+++ crewMember.js
@@ -3,4 +3,31 @@ class CrewMember {
     this.position = member
     this.currentShip = "Looking for a Rig"
   }
+
+  engageWarpDrive(){
+    if(this.currentShip == 'Looking for a Rig'){
+      return "had no effect"
+    }
+    else{
+      return
+    }
+  }
+
+  setsInvisibility(){
+    if(this.currentShip == 'Looking for a Rig'){
+      return "had no effect"
+    }
+    else{
+      return
+    }
+  }
+
+  chargePhasers(){
+    if(this.currentShip == 'Looking for a Rig'){
+      return "had no effect"
+    }
+    else{
+      return
+    }
+  }
 }
