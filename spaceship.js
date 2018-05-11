17f54f17c2810ad0c8024642547b3161871f7864
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 22:12:32 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -6,7 +6,7 @@ class Spaceship {
     this.shields = shields
     this.cloaked = false
     this.warpDrive = 'disengaged'
-    this.docked = false
+    this.docked = true
     this.phasersCharge = 'uncharged'
   }
 }
