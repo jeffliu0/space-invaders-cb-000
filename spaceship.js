5be10adcfa6f04512cf180971ce5a49da64f0710
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 22:13:58 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -1,7 +1,7 @@
 class Spaceship {
   constructor(name, team, phasers, shields){
     this.name = name
-    this.team = team
+    this.member = team
     this.phasers = phasers
     this.shields = shields
     this.cloaked = false
